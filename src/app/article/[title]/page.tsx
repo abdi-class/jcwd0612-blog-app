a27bad90ed9ae833cd04b58f1c097dbe2d700148
Abdi-01 <abdialghi@gmail.com>
2025-07-03 09:17:31 +0700

    ssr

--- src/app/article/[title]/page.tsx
+++ src/app/article/[title]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { apiCall } from "@/helper/apiCall";
 import * as React from "react";
 
@@ -6,29 +5,26 @@ interface IArticleDetailPageProps {
   params: { title: string };
 }
 
-const ArticleDetailPage: React.FunctionComponent<IArticleDetailPageProps> = (
-  props
-) => {
-  const [detail, setDetail] = React.useState<any>(null);
-  const getDetail = async () => {
-    try {
-      const res = await apiCall.get(
-        `/articles?where=%60title%60%20%3D%20'${props.params.title}'`
-      );
+const getDetail = async (title: string) => {
+  try {
+    const res = await apiCall.get(
+      `/articles?where=%60title%60%20%3D%20'${title}'`
+    );
+    console.log(res.data[0]);
 
-      console.log(res.data);
-      setDetail(res.data[0]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return res.data[0];
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  React.useEffect(() => {
-    getDetail();
-  }, []);
+const ArticleDetailPage: React.FunctionComponent<
+  IArticleDetailPageProps
+> = async (props) => {
+  const detailData = await getDetail(props.params.title);
   return (
     <div>
-      <h1 className="text-5xl">{detail?.title}</h1>
+      <h1 className="text-5xl">{detailData?.title}</h1>
     </div>
   );
 };
